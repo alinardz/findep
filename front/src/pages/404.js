6e07aace93de498a53182fa753279ee0866fc3c9
alinardz <alinagriselrodriguez@gmail.com>
2019-11-06 22:40:52 -0600

    Feature: sasslint, eslint and husky

--- front/src/pages/404.js
+++ front/src/pages/404.js
@@ -1,7 +1,8 @@
-import React from "react"
+/* eslint-disable react/jsx-pascal-case */
+import React from 'react'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 const NotFoundPage = () => (
   <Layout>
--- front/src/pages/page-2.js
+++ front/src/pages/page-2.js
@@ -1,12 +1,13 @@
-import React from "react"
-import { Link } from "gatsby"
+/* eslint-disable react/jsx-pascal-case */
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
+    <SEO title="Page two" />// eslint-disable-next-line react/jsx-pascal-case
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
